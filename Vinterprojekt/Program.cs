55fe674c0d09299634e5564766421e24d9d71526
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2022-01-12 13:12:34 +0100

    start

--- Vinterprojekt/Program.cs
+++ Vinterprojekt/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace Vinterprojekt
+Console.WriteLine("Hello and Welcome the world of Pokemon, a place where both humans and mythical creatures can come togethor. This is the Tunod region. One of the many places in the pokemon world.");
+Console.ReadLine();
+
+
+string gender = "";
+
+while ( gender != "Boy" && gender != "Girl" )
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    Console.WriteLine("Are you a boy or a girl?");
+    gender = Console.ReadLine();
 }
+
+Console.WriteLine("Let's Begin with your name, what is it?");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Ok so your name is {name}");
+Console.ReadLine();
+
+Console.WriteLine("This is my nephew. You guys have known eachother since you were both babies");
+Console.WriteLine("Umm what was his name again?");
+string rival = Console.ReadLine();
+
+Console.WriteLine($"Ahh I remember now his name was {rival}");
+Console.ReadLine();
+
+
+
+Console.ReadLine();
+
