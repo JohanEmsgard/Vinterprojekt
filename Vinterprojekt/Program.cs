64edfa88e4c84c6d59af91302d36348bded59613
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2022-01-25 12:41:54 +0100

    uherwuy

--- Vinterprojekt/Program.cs
+++ Vinterprojekt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 Console.WriteLine("Hello and Welcome the world of Pokemon, a place where both humans and mythical creatures can come togethor. This is the Tunod region. One of the many places in the pokemon world.");
 Console.ReadLine();
 
